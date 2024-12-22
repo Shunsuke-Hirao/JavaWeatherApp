e0d3c17605f6f0cbc77e45618d033599b5b6b68e
i-am-shunsuke <200557414@student.georgianc.on.ca>
2024-12-22 03:15:08 -0500

    added exception

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -13,15 +13,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-
-//    import from 54:56
-
-//    39:27
 
     TextView cityName;
     Button search;
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     String url;
 
-    class getWeather extends AsyncTask<String, void,String>{
+    class getWeather extends AsyncTask<String, Void,String>{
 
         @Override
         protected String doInBackground(String... urls) {
@@ -38,6 +40,40 @@ public class MainActivity extends AppCompatActivity {
                 URL url=new URL(urls[0]);
                 HttpURLConnection urlConnection=(HttpURLConnection) url.openConnection();
                 urlConnection.connect();
+
+                InputStream inputStream=urlConnection.getInputStream();
+                BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
+
+                String line="";
+                while((line=reader.readLine())!=null){
+                    result.append(line).append("\n");
+                }
+                return result.toString();
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            try{
+                JSONObject jsonObject=new JSONObject(result);
+                String weatherInfo=jsonObject.getString("main");
+                weatherInfo=weatherInfo.replace("temp","Temperature");
+                weatherInfo=weatherInfo.replace("feel_like","Feels like");
+                weatherInfo=weatherInfo.replace("temp_max","Temperature Max");
+                weatherInfo=weatherInfo.replace("temp_min","Temperature Min");
+                weatherInfo=weatherInfo.replace("pressure","Pressure");
+                weatherInfo=weatherInfo.replace("humidity","Humidity");
+                weatherInfo=weatherInfo.replace("{","");
+                weatherInfo=weatherInfo.replace("}","");
+                weatherInfo=weatherInfo.replace(",","\n");
+                weatherInfo=weatherInfo.replace(":"," : ");
+
+                show.setText(weatherInfo);
+            }catch (Exception e){
+                e.printStackTrace();
             }
         }
     }
@@ -65,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     getWeather task=new getWeather();
                     temp[0]=task.execute(url).get();
-                }catch (Exception e){
+                }catch (ExecutionException e){
                     e.printStackTrace();
                 }catch (InterruptedException e){
                     e.printStackTrace();
